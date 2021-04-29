ad78beb673b52fa4cec24a3c64a82f893dfe95eb
ManUnDead <doomlok15@mail.ru>
2021-04-29 18:15:45 +0300

    #3.75 New button WOW!
    Created bnew button , that spawn minion when u touch it

---
+++ Assets/Sсripts/SpawnTurbo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTurbo : MonoBehaviour
+{
+    public Transform SpawnPos;
+    public GameObject SpawnObject;
+    public float TimeSpawn;
+    void Start()
+    {
+        
+    }
+
+
+   private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+            Instantiate(SpawnObject, SpawnPos.position, Quaternion.identity);
+    }
+}
