f222e8d443183bb74f82a43c061dadbf156cd97d
ManUnDead <doomlok15@mail.ru>
2021-05-03 21:49:57 +0300

    3.81 Now 3lvl right box can resp some money
    TAKE IT OR NOT!

---
+++ Assets/Sсripts/Spawn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public Transform SpawnPos;
+    public GameObject SpawnObject;
+    public float TimeSpawn;
+
+    int GetMoney = 0;
+    public GameObject money;
+    public Transform moneyPos;
+
+    void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+     
+    }
+
+   private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Instantiate(SpawnObject, SpawnPos.position, Quaternion.identity);
+            GetMoney++;
+            StartCoroutine(SpawnMoney());
+        }
+
+    }
+
+    IEnumerator SpawnMoney()
+    {
+        if (GetMoney % 2 != 0)
+        Instantiate(money, moneyPos.position, Quaternion.identity);   
+        yield return new WaitForSeconds(1f);
+        StopCoroutine(SpawnMoney());
+    }
+}
