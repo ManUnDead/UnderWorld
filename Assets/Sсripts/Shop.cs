db084d70b930915b7eaf3e9e3ed89ad56da6db0f
ManUnDead <doomlok15@mail.ru>
2021-05-07 14:07:57 +0300

    #3.92 Now u can buy and select skins

--- Assets/Sсripts/Shop.cs
+++ Assets/Sсripts/Shop.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
+    public GameObject BuyButton;
+    int BuySkin;
+    int coins;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("BuySkin"))
+        {
+            BuySkin=2;
+        }
+
+        BuySkin = PlayerPrefs.GetInt("BuySkin", 1);
+        coins = PlayerPrefs.GetInt("Coins");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (BuySkin ==1)
+        {
+            BuyButton.SetActive(true);
+        }
+
+        else
+        {
+            BuyButton.SetActive(false);
+        }
+    }
+     public void BuySkins()
+    {
+        if (coins >= 50)
+        {
+            coins -= 50;
+            PlayerPrefs.SetInt("Coins",coins);
+            BuySkin = 2;
+            PlayerPrefs.GetInt("BuySkin", BuySkin);
+            PlayerPrefs.SetInt("BuySkin", BuySkin);
+        }
     }
 }
